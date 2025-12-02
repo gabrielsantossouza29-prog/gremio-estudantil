db7959c28f9fcc66f40ffe48ff4ed764112cc4a9
gabrielsantossouza29-prog <gabriel.santos.souza29@escola.pr.gov.br>
2025-12-02 20:16:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function tocaSom(idElentoaudio){
+const elemento == ducument . query Selector (idElemento audio) 
+ if (elemento &&elemento. local Name===audio
+        elemento . play( ),
+ [else
+       console . log9 elemento não enconra'                                                                                                                                                                     
+ ]
